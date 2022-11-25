35a9c02517340570cd803f30e645500aa4e7bcba
chan <bread06053@naver.com>
2022-11-25 16:48:25 +0900

    push2

---
+++ projectsugar/src/main/java/spring/example/ProjectsugarApplication.java
@@ -0,0 +1,13 @@
+package spring.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsugarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsugarApplication.class, args);
+	}
+
+}
